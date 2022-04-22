172ace7cb4c1a7f163014fcdd1fcad1c59f2e9bd
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-22 10:41:59 +0600

    Connect with MongoDB with server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,32 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oiekl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db('travelGeeks').collection('services');
+
+    app.get('/service', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+
+      res.send(services);
+    });
+  } finally {
+    console.log('Completed');
+  }
+}
+
+run().catch(console.dir);
 
 // Root API
 app.get('/', (req, res) => {
