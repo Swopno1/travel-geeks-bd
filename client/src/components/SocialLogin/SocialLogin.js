80ffcea96d178a611809e5b06f9c6bb7aa6f7234
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-26 13:26:27 +0600

    Fix login From navigation in social login

--- client/src/components/SocialLogin/SocialLogin.js
+++ client/src/components/SocialLogin/SocialLogin.js
@@ -4,12 +4,15 @@ import {
   useSignInWithGoogle,
 } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Loading from '../Loading/Loading';
 
 const SocialLogin = () => {
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
   const [signInWithGithub, user1, loading1, error1] = useSignInWithGithub(auth);
+  const location = useLocation();
+
+  let from = location.state?.from?.pathname || '/';
   const navigate = useNavigate();
 
   let errorElement = '';
@@ -27,7 +30,7 @@ const SocialLogin = () => {
   }
 
   if (user || user1) {
-    navigate('/home');
+    navigate(from ? from : '/', { replace: true });
   }
 
   return (
